c803c621e55e5437be13603de41b0c657171be7e
kovilev98 <kovilevi98@gmail.com>
2020-02-26 23:07:38 +0100

    webview works

--- app/src/main/java/hu/bme/onlab/mybrowser/MainActivity.kt
+++ app/src/main/java/hu/bme/onlab/mybrowser/MainActivity.kt
@@ -1,12 +1,21 @@
 package hu.bme.onlab.mybrowser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timer().schedule(3000){
+            val intent = Intent(applicationContext,WebViewActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/hu/bme/onlab/mybrowser/WebViewActivity.kt
@@ -0,0 +1,161 @@
+package hu.bme.onlab.mybrowser
+
+import android.animation.ValueAnimator
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.content.Intent
+import android.media.audiofx.BassBoost
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.Transformation
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_web_view.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
+
+
+class WebViewActivity : AppCompatActivity() {
+        private var URL = "https://google.com"
+        private var isAlreadyCreated = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+
+
+       // startLoaderAnimate()
+
+        webView.settings.javaScriptEnabled =true
+        webView.settings.setSupportZoom(false)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                //       endLoaderAnimate()
+            }
+
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                super.onReceivedError(view, request, error)
+                //endLoaderAnimate()
+                showErrorDialog("Error",
+                    "No internet connection. Please check your connection.",
+                    this@WebViewActivity)
+            }
+        }
+
+        webView.loadUrl(URL)
+
+    }
+
+    //setting menu in action bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // actions on click menu items
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_cut -> {
+            // User chose the "Print" item
+            Toast.makeText(this,"Action cut",Toast.LENGTH_LONG).show()
+            true
+        }
+        android.R.id.home ->{
+            Toast.makeText(this,"Home action",Toast.LENGTH_LONG).show()
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (isAlreadyCreated && !isNetworkAvailable()) {
+            isAlreadyCreated = false
+            showErrorDialog("Error", "No internet connection. Please check your connection.",
+                this@WebViewActivity)
+        }
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectionManager =
+            this@WebViewActivity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectionManager.activeNetworkInfo
+
+        return networkInfo != null && networkInfo.isConnectedOrConnecting
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun showErrorDialog(title: String, message: String, context: Context) {
+        val dialog = AlertDialog.Builder(context)
+        dialog.setTitle(title)
+        dialog.setMessage(message)
+        dialog.setNegativeButton("Cancel", { _, _ ->
+            this@WebViewActivity.finish()
+        })
+
+        dialog.setPositiveButton("Retry", { _, _ ->
+            this@WebViewActivity.recreate()
+        })
+        dialog.create().show()
+    }
+
+    /*private fun endLoaderAnimate() {
+        loaderImage.clearAnimation()
+        loaderImage.visibility = View.GONE
+    }
+
+    private fun startLoaderAnimate() {
+        val objectAnimator = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+                val startHeight = 170
+                val newHeight = (startHeight + (startHeight + 40) * interpolatedTime).toInt()
+                loaderImage.layoutParams.height = newHeight
+                loaderImage.requestLayout()
+            }
+
+            override fun initialize(width: Int, height: Int, parentWidth: Int, parentHeight: Int) {
+                super.initialize(width, height, parentWidth, parentHeight)
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        objectAnimator.repeatCount = -1
+        objectAnimator.repeatMode = ValueAnimator.REVERSE
+        objectAnimator.duration = 1000
+        loaderImage.startAnimation(objectAnimator)
+    }*/
+
+
+}
